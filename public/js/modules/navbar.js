211d64144dd9727471a41a664173404fddcd7da3
Schalk van Dyk <schalkvandyk1979@gmail.com>
2024-12-27 11:20:37 +0200

    add navItem animation to navbar__menu-item

--- public/js/modules/navbar.js
+++ public/js/modules/navbar.js
@@ -1,16 +1,26 @@
-// variables for hamburgerButton, navbarMenu and hamburger icon
-const hamburgerButton = document.querySelector(".navbar__hamburger");
-const navbarMenu = document.querySelector(".navbar__menu");
-const icon = hamburgerButton.querySelector(".navbar__hamburger-icon");
-
 // navbar logic to toggle between hamburger btn and navabar menu
 const navbarLogic = () => {
       document.addEventListener("DOMContentLoaded", function() {
+            // variables for hamburgerButton, navbarMenu and hamburger icon
+            const hamburgerButton = document.querySelector(".navbar__hamburger");
+            const navbarMenu = document.querySelector(".navbar__menu");
+            const navItems = document.querySelectorAll(".navbar__menu-item");
+            const icon = hamburgerButton.querySelector(".navbar__hamburger-icon");
             hamburgerButton.addEventListener("click", function() {
-                navbarMenu.classList.toggle("active");
-                navbarMenu.classList.toggle("navbarMenuOpenAnimation");
-                icon.classList.toggle("active");
-
+                  navbarMenu.classList.toggle("active");
+                  navbarMenu.classList.toggle("navbarMenuOpenAnimation");
+                  let counter = 0;
+                  for(let item of navItems) {
+                        if (item.classList.contains("navItemMoveInAnimation")) {
+                              item.classList.toggle("navItemMoveInAnimation");
+                        } else {
+                              counter = 200 + counter;
+                              setTimeout(function() {
+                                    item.classList.toggle("navItemMoveInAnimation");
+                              }, (1500 + counter))
+                        }
+                  }
+                  icon.classList.toggle("active");
             });
       });
 }
